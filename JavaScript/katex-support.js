2d624dab8bc339c6b2d102d9bb384dbab54f8c38
Nicholas Selby <rupumped@gmail.com>
2024-11-18 12:17:46 +0300

    Updated katex support for new version

--- JavaScript/katex-support.js
+++ JavaScript/katex-support.js
@@ -8,42 +8,34 @@ window.addEventListener('DOMContentLoaded', () => {
 	})
 })
 
-// The following script is adapted from Murray Bourne's "KaTeX tag placement with no wrap"
-// https://bourne2learn.com/math/katex/tag-placement_nowrap.html
-
-function adjustTag() {
-	var browserWidth = document.querySelector('body').clientWidth;
-	var eqnWidth = 0;
-	var tagWidth = 0;
-
-	var tags = document.querySelectorAll('.katex-html .tag');	
-	
-	for(i=0;i<tags.length;i++) {
-		eqnWidth = tags[i].parentNode.querySelector('.base').offsetWidth;
-		tagWidth = tags[i].clientWidth;
+// To prevent overlap between equation tags and equation bases
+function adjustKatexTags() {
+	const equations = document.querySelectorAll('.katex-html:has(> .base):has(> .tag)');
+	equations.forEach(eq => {
+		const base = eq.querySelector('.base');
+		const tag = eq.querySelector('.tag');
+				
+		// Get bounding rectangles
+		const baseRect = base.getBoundingClientRect();
+		const tagRect = tag.getBoundingClientRect();
 		
-		tags[i].style.border = "";
+		// Check for overlap
+		const hasOverlap = !(
+			baseRect.right < tagRect.left ||
+			baseRect.left > tagRect.right ||
+			baseRect.bottom < tagRect.top ||
+			baseRect.top > tagRect.bottom
+		);
 		
-		if(1.1*eqnWidth + 2*tagWidth > browserWidth) {
-			tags[i].style.display = "inline-block";
-			tags[i].style.marginLeft = "8px";
-			tags[i].style.position = "static";
-		} else {
-			tags[i].style.background = "";
-			tags[i].style.position = "absolute";
-			tags[i].style.right = 0;
+		if (hasOverlap) {
+			// Switch to flex layout only if there's overlap
+			eq.style.display = 'flex';
+			eq.style.justifyContent = 'space-between';
+			eq.style.alignItems = 'center';
+			tag.style.position = 'static';
+			console.log('here')
 		}
-	}
+	});
 }
-
-function waitKaTeX() {
-	if(document.querySelectorAll('.katex-html').length == 0 ) {
-		requestAnimationFrame(waitKaTeX);
-	} else {
-		cancelAnimationFrame(raf);
-		adjustTag();
-	}
-}
-var raf = window.requestAnimationFrame(waitKaTeX);
-
-window.addEventListener("resize", adjustTag);
+window.addEventListener('DOMContentLoaded', adjustKatexTags);
+window.addEventListener('resize', adjustKatexTags);
